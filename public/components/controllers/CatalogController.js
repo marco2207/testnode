1c464f3d448e917326ecf17d3149e225432bd733
marco2207 <drake2207@gmail.com>
2018-04-19 13:31:30 +0200

    Add files via upload

---
+++ public/components/controllers/CatalogController.js
@@ -0,0 +1,16 @@
+app.controller('CatalogController', ['$scope','BlueAPIService','UserInfoService',function($scope, BlueAPIService, UserInfoService) {
+
+	console.log("Entering Catalog Controller")
+	$scope.baseURL = "images/items/"
+	$scope.loggedIn = UserInfoService.state.authenticated
+	console.log("--> Catalog Controller items baseURL: " + $scope.baseURL)
+
+	BlueAPIService.getCatalog(function (response) {
+			console.log("Get Catalog Result" + response)
+			$scope.itemList = response.data
+
+		}, function (error){
+			console.log("Get Inventory Error: " + error);
+	});
+
+}]);
