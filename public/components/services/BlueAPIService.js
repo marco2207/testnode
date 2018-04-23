fdcfa9c5a25f6657d54c4581cd02831d95fb58a7
Microclimate User <microclimate.user@localhost>
2018-04-23 17:11:19 +0200

    Changed BlueAPIService

--- public/components/services/BlueAPIService.js
+++ public/components/services/BlueAPIService.js
@@ -1,5 +1,5 @@
 //app.service('BlueAPIService',['$http', 'CONFIG', '$base64', function($http, CONFIG, $base64) {
-app.service('BlueAPIService',['$http', '$base64', function($http, CONFIG, $base64) {
+app.service('BlueAPIService',['$http', '$base64', function($http, $base64) {
 
 	var invokeService = function(restUrl,
 			requestType, parameters, successCallback,
